d01f92feb2f802db1794a208c5ab828c3219466d
Benoit Foujols <benoit@foujols.com>
2022-05-28 23:42:07 +0200

    #3 build(core) #done #comment add structure package .phar

--- src/Command/CreateStudentCommand.php
+++ src/Command/CreateStudentCommand.php
@@ -1,8 +1,8 @@
 <?php
 
-namespace app\Command;
+namespace ManageStudent\Command;
 
-use app\Core\CommandOption;
+use ManageStudent\Core\CommandOption;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
