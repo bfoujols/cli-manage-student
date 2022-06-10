09cfb70eb9738bdba3ad051f9cbd47b53c99de6d
Benoit Foujols <benoit@foujols.com>
2022-06-10 21:25:17 +0200

    #9 #done #comment Add check info

--- src/App.php
+++ src/App.php
@@ -7,7 +7,7 @@ class App extends Application
 {
     public function __construct()
     {
-        parent::__construct("Manage Student CLI", "1.0.0@dev");
+        parent::__construct("Manage Student CLI", "0.1.0@alpha");
 
         $this->add(new \ManageStudent\Command\DefaultCommand());
         $this->add(new \ManageStudent\Command\CreateStudentCommand());
