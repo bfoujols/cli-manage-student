adc4201be4ba60b49a6bd28e91bfe5600be2e77c
Benoit Foujols <benoit@foujols.com>
2022-06-28 16:51:35 +0200

    Ajout de test sur les espaces

--- tests/Service/NomanclatureServiceTest.php
+++ tests/Service/NomanclatureServiceTest.php
@@ -4,6 +4,8 @@ namespace Service;
 
 use DateTime;
 use DateTimeZone;
+use ManageStudent\Entity\Student;
+use ManageStudent\Service\NomanclatureService;
 use PHPUnit\Framework\TestCase;
 
 class NomanclatureServiceTest extends TestCase
@@ -12,13 +14,12 @@ class NomanclatureServiceTest extends TestCase
     /**
      * @test FileNom01
      */
-    public function testNom01DateToString() : void
+    public function testNom01DateToString(): void
     {
-        $student = new \ManageStudent\Entity\Student();
-        $dateNow = new DateTime('2017-10-17', new DateTimeZone('Europe/Paris'));
-        $student->setDateNaissance($dateNow);
+        $student = new Student();
+        $student->setDateNaissance((new DateTime('2017-10-17', new DateTimeZone('Europe/Paris'))));
         $this->assertIsString(
-            (new \ManageStudent\Service\NomanclatureService)->getDateToString($student->getDateNaissance())
+            (new NomanclatureService)->getDateToString($student->getDateNaissance())
         );
     }
 
@@ -27,14 +28,13 @@ class NomanclatureServiceTest extends TestCase
      */
     public function testNom02IsString() : void
     {
-        $student = new \ManageStudent\Entity\Student();
-        $student->setNom("Ort");
-        $student->setPrenom("Montreuil");
-        $dateNow = new DateTime('2017-10-17', new DateTimeZone('Europe/Paris'));
-        $student->setDateNaissance($dateNow);
+        $student = new Student();
+        $student->setNom("Babutta");
+        $student->setPrenom("Tom");
+        $student->setDateNaissance((new DateTime('2017-10-17', new DateTimeZone('Europe/Paris'))));
 
         $this->assertIsString(
-            (new \ManageStudent\Service\NomanclatureService)->getNameWithoutType($student)
+            (new NomanclatureService)->getNameWithoutType($student)
         );
 
     }
@@ -44,17 +44,113 @@ class NomanclatureServiceTest extends TestCase
      */
     public function testNom03WhithoutType() : void
     {
-        $student = new \ManageStudent\Entity\Student();
+        $student = new Student();
 
-        $student->setNom("Ort");
-        $student->setPrenom("Montreuil");
+        $student->setNom("Babutta");
+        $student->setPrenom("Tom");
 
         $dateNow = new DateTime('1980-10-17', new DateTimeZone('Europe/Paris'));
         $student->setDateNaissance($dateNow);
 
         $this->assertSame(
-            "ORT-Montreuil-801017",
-            (new \ManageStudent\Service\NomanclatureService)->getNameWithoutType($student)
+            "BABUTTA-Tom-801017",
+            (new NomanclatureService)->getNameWithoutType($student)
         );
     }
+
+    /**
+     * @test FileNom04
+     */
+    public function testNom04FullUpper(): void
+    {
+        $student = new Student();
+
+        $student->setNom("BABUTTA");
+        $student->setPrenom("TOM");
+
+        $dateNow = new DateTime('1980-10-17', new DateTimeZone('Europe/Paris'));
+        $student->setDateNaissance($dateNow);
+
+        $this->assertSame(
+            "BABUTTA-Tom-801017",
+            (new NomanclatureService)->getNameWithoutType($student)
+        );
+    }
+
+    /**
+     * @test FileNom05
+     */
+    public function testNom05WhitoutUpper(): void
+    {
+        $student = new Student();
+
+        $student->setNom("babutta");
+        $student->setPrenom("tom");
+
+        $dateNow = new DateTime('1980-10-17', new DateTimeZone('Europe/Paris'));
+        $student->setDateNaissance($dateNow);
+
+        $this->assertSame(
+            "BABUTTA-Tom-801017",
+            (new NomanclatureService)->getNameWithoutType($student)
+        );
+    }
+
+    /**
+     * @test FileNom06
+     */
+    public function testNom06WhitoutBlank(): void
+    {
+        $student = new Student();
+
+        $student->setNom(" Babutta");
+        $student->setPrenom("Tom");
+
+        $dateNow = new DateTime('1980-10-17', new DateTimeZone('Europe/Paris'));
+        $student->setDateNaissance($dateNow);
+
+        $this->assertSame(
+            "BABUTTA-Tom-801017",
+            (new NomanclatureService)->getNameWithoutType($student)
+        );
+    }
+
+    /**
+     * @test FileNom07
+     */
+    public function testNom07WhitoutBlank(): void
+    {
+        $student = new Student();
+
+        $student->setNom("Babu tta");
+        $student->setPrenom("Tom");
+
+        $dateNow = new DateTime('1980-10-17', new DateTimeZone('Europe/Paris'));
+        $student->setDateNaissance($dateNow);
+
+        $this->assertSame(
+            "BABU-TTA-Tom-801017",
+            (new NomanclatureService)->getNameWithoutType($student)
+        );
+    }
+
+    /**
+     * @test FileNom08
+     */
+    public function testNom08WhitoutManyBlank(): void
+    {
+        $student = new Student();
+
+        $student->setNom("  Babutta ");
+        $student->setPrenom(" Tom  ");
+
+        $dateNow = new DateTime('1980-10-17', new DateTimeZone('Europe/Paris'));
+        $student->setDateNaissance($dateNow);
+
+        $this->assertSame(
+            "BABUTTA-Tom-801017",
+            (new NomanclatureService)->getNameWithoutType($student)
+        );
+    }
+
 }
