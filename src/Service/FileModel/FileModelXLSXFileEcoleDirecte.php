0b20decd1567f8747b56b33abd497d35fa99ea2b
Benoit Foujols <benoit@foujols.com>
2022-07-02 23:33:29 +0200

    #52 #done #comment Add new format import fichier Ecole Directe

---
+++ src/Service/FileModel/FileModelXLSXFileEcoleDirecte.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace ManageStudent\Service\FileModel;
+
+use DateTime;
+use ManageStudent\Entity\Student;
+use ManageStudent\Exception\DateInvalideErrorException;
+use ManageStudent\Service\DateService;
+
+class FileModelXLSXFileEcoleDirecte extends FileModel implements FileModelInterface
+{
+
+    public function analyse(array $structure): bool
+    {
+        $state = false;
+
+        if (str_contains($structure[0][0], "Edité le") === true
+            && str_contains($structure[0][4], "Année scolaire") === true
+            && str_contains($structure[2][1], "Classe") === true
+            && str_contains($structure[4][1], "Effectif") === true) {
+
+            self::$arrFileModel = $structure;
+            $state = true;
+        }
+
+        return $state;
+    }
+
+    /**
+     * @throws DateInvalideErrorException|\Exception
+     */
+    public function getStudents(): array
+    {
+        $tabStudent = [];
+
+        foreach (self::$arrFileModel as $item => $student) {
+            try {
+                if ($item >= 7) {
+                    $newStudent = new Student();
+                    $newStudent = $this->splitStudentName($student[0], $newStudent);
+
+                    if ((new DateService())->isValid($student[1], "d/m/Y") === true) {
+                        $newStudent->setDateNaissance(DateTime::createFromFormat("d/m/Y", $student[1]));
+                    } else {
+                        throw new DateInvalideErrorException();
+                    }
+                    $tabStudent[] = $newStudent;
+                }
+            } catch (DateInvalideErrorException $exception) {
+                // Todo Exception
+                echo "Date invalide " . $student[1];
+                exit;
+            }
+        }
+
+
+        return $tabStudent;
+    }
+
+    public function getNameModel(): string
+    {
+        return "Fichier XLSX Export by Ecole Directe";
+    }
+
+    private function splitStudentName(string $fullName, Student $student): Student
+    {
+        $nom = "";
+        $prenom = "";
+
+        // Split avec un espace les mots
+        $tabName = explode(" ", $fullName);
+        // Boucle pour trier le mot en majuscule (nom) et miniscule (Prenom)
+        foreach ($tabName as $nameRaw) {
+            (preg_match('/^[A-Z0-9]+$/', $nameRaw)) ?
+                $nom .= " " . $nameRaw :
+                $prenom .= " " . $nameRaw;
+        }
+
+        $student->setPrenom($prenom)->setNom($nom);
+
+        return $student;
+    }
+}
