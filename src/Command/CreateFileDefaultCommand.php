566f3b7f67ebfc924740afe8c19cf26188904867
Benoit Foujols <benoit@foujols.com>
2022-07-07 13:00:42 +0200

    #59 Fix Codacy Check warning

--- src/Command/CreateFileDefaultCommand.php
+++ src/Command/CreateFileDefaultCommand.php
@@ -6,10 +6,9 @@ use ManageStudent\Service\Command\CommandBanner;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\Filesystem\Filesystem;
 
-class CreateFileDefaultCommand extends Command
+class CreateFileDefaultCommand extends CommandManage
 {
     protected static $defaultName = 'file:default';
     protected static $defaultDescription = 'Création du fichier par défaut';
@@ -17,28 +16,27 @@ class CreateFileDefaultCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $render = new SymfonyStyle($input, $output);
-        $render->writeln(CommandBanner::getBanner());
-        $render->writeln([
+        self::$stdOutput->writeln([
+            CommandBanner::getBanner(),
             'Command : ' . self::$defaultName
         ]);
 
         $filesystem = new Filesystem();
         if ($filesystem->exists(getcwd() . "/liste-etudiant.xlsx")) {
-            $render->writeln([
+            self::$stdOutput->writeln([
                 '',
                 '<error>[X] Le fichier est deja present dans le repertoire</error>',
                 ''
             ]);
-        } else {
-            $filesystem->copy(__DIR__ . "/../../ressources/liste-etudiant.xlsx", getcwd() . "/liste-etudiant.xlsx");
-            $render->writeln([
-                '',
-                '<info>[*] Le fichier est créé liste-etudiant.xlsx</info>',
-                ''
-            ]);
+            return Command::SUCCESS;
         }
 
+        $filesystem->copy(__DIR__ . "/../../ressources/liste-etudiant.xlsx", getcwd() . "/liste-etudiant.xlsx");
+        self::$stdOutput->writeln([
+            '',
+            '<info>[*] Le fichier est créé liste-etudiant.xlsx</info>',
+            ''
+        ]);
         return Command::SUCCESS;
     }
 }
