c345c1597d26daf8a2c150979b20fe75aa2af361
Benoit Foujols <benoit@foujols.com>
2022-08-20 23:32:01 +0200

    #70 #done #comment Fix Hash

--- tests/Command/CommandFileDefaultTest.php
+++ tests/Command/CommandFileDefaultTest.php
@@ -34,7 +34,7 @@ final class CommandFileDefaultTest extends TestCase
     {
         $this->commandeTester->execute([]);
         $this->assertSame(
-            "4cf886e7c52c2d3987099df7b3026e5edc6ecf93b6ec3818c34d1ecb890e91da",
+            "ee5cda1ffec25948635f53ed8ca6e6e8fc5c37b1a5e99cea37cbf35a0c0284c2",
             hash("sha256", $this->commandeTester->getDisplay())
         );
     }
@@ -46,7 +46,7 @@ final class CommandFileDefaultTest extends TestCase
     {
         $this->commandeTester->execute([]);
         $this->assertSame(
-            "3d3c98d44c27170a3ae07851161c2ce40e929e0d6e88e943596674ef0c2f2cea",
+            "7cfcb3b700fd406b24ef39b7196e276e2881349996fbafc25d59ba203f84834e",
             hash("sha256", $this->commandeTester->getDisplay())
         );
         // Purge du fichier tester
