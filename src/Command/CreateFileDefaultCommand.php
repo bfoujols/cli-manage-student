31b5a7050be20009a69f1e7c23dd8a0a4954e173
Benoit Foujols <benoit@foujols.com>
2022-09-05 22:18:03 +0200

    #75 #in_progress #comment Refactoring de la commande par defaut

--- src/Command/CreateFileDefaultCommand.php
+++ src/Command/CreateFileDefaultCommand.php
@@ -3,6 +3,7 @@
 namespace Studoo\Command;
 
 use Studoo\Service\Command\CommandBanner;
+use Studoo\Service\Command\QuestionType;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -12,6 +13,15 @@ class CreateFileDefaultCommand extends CommandManage
 {
     protected static $defaultName = 'file:default';
     protected static $defaultDescription = 'Création du fichier par défaut';
+    private $fileDefaut = array(
+        "0" => "Fichier liste des étudiants au format XLSX"
+    );
+    private $fileDefautConfig = array(
+        "0" => [
+            "file" => "liste-etudiant.xlsx",
+            "fileRessource" => "/../../ressources/liste-etudiant.xlsx"
+        ]
+    );
 
 
     protected function execute(InputInterface $input, OutputInterface $output): int
@@ -21,22 +31,30 @@ class CreateFileDefaultCommand extends CommandManage
             'Command : ' . self::$defaultName
         ]);
 
-        $filesystem = new Filesystem();
-        if ($filesystem->exists(getcwd() . "/liste-etudiant.xlsx")) {
+        $choiceFile = new QuestionType($input, $output);
+        $finalChoiseFile = $choiceFile->Choice("Quel fichier par défaut, voulez vous ?", $this->fileDefaut, 'Merci de choisir un fichier');
+        $keyFileDefaut = array_search($finalChoiseFile, $this->fileDefaut);
+
+
+        if (array_key_exists($keyFileDefaut, $this->fileDefautConfig)) {
+            $filesystem = new Filesystem();
+            if ($filesystem->exists(getcwd() . "/" . $this->fileDefautConfig[$keyFileDefaut]["file"])) {
+                self::$stdOutput->writeln([
+                    '',
+                    '<error>[X] Le fichier est deja present dans le repertoire</error>',
+                    ''
+                ]);
+                return Command::SUCCESS;
+            }
+
+            $filesystem->copy(__DIR__ . $this->fileDefautConfig[$keyFileDefaut]["fileRessource"], getcwd() . "/" . $this->fileDefautConfig[$keyFileDefaut]["file"]);
             self::$stdOutput->writeln([
                 '',
-                '<error>[X] Le fichier est deja present dans le repertoire</error>',
+                '<info>[*] Le fichier est créé ' . $this->fileDefautConfig[$keyFileDefaut]["file"] . '</info>',
                 ''
             ]);
             return Command::SUCCESS;
         }
-
-        $filesystem->copy(__DIR__ . "/../../ressources/liste-etudiant.xlsx", getcwd() . "/liste-etudiant.xlsx");
-        self::$stdOutput->writeln([
-            '',
-            '<info>[*] Le fichier est créé liste-etudiant.xlsx</info>',
-            ''
-        ]);
-        return Command::SUCCESS;
+        return Command::FAILURE;
     }
 }
