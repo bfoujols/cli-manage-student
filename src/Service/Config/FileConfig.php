232598e41d805a82ab53ee8bd0edf147bb662328
Benoit Foujols <benoit@foujols.com>
2022-11-13 21:57:27 +0100

    #81 #in_progress #comment Refactoring FileConfig

--- src/Service/Config/FileConfig.php
+++ src/Service/Config/FileConfig.php
@@ -1,23 +1,31 @@
 <?php
+/*
+ * Ce fichier fait partie du projet Studoo.
+ *
+ * (c) Benoit Foujols <Benoit.Foujols@ac-creteil.fr>
+ *
+ * Pour les informations complètes sur les droits d'auteur et la licence,
+ * veuillez consulter le fichier LICENSE qui a été distribué avec ce code source.
+ */
 
 namespace Studoo\Service\Config;
 
+/**
+ * Gestion des fichiers de configuration
+ */
 class FileConfig
 {
     private string $fileName = "studoo";
     private string $extConfig = ".json";
-    private string $extStudend = "-db.json";
     private string $extLock = ".lock";
     protected string $fileLock;
     protected string $fileConfig;
-    protected string $fileStudend;
     protected string $formatDateTime = 'Y-m-d H:i:s';
 
     public function __construct()
     {
         $this->fileLock = $this->fileName . $this->extLock;
         $this->fileConfig = $this->fileName . $this->extConfig;
-        $this->fileStudend = $this->fileName . $this->extStudend;
     }
 
 }
